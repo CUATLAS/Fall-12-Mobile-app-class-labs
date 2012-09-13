dce2809cc1ce4f14e9c66bbe2f744fdc735501ab
Elise Beall <elisebeall@gmail.com>
2012-09-13 15:30:28 -0600

    2012.09.13

---
+++ beatles_elisebeall/beatles_elisebeall/MADViewController.h
@@ -0,0 +1,13 @@
+//
+//  MADViewController.h
+//  beatles_elisebeall
+//
+//  Created by Elise J Beall on 9/13/12.
+//  Copyright (c) 2012 Elise J Beall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MADViewController : UIViewController
+
+@end
---
+++ elise_beall/beatles/beatles/MADViewController.m
@@ -0,0 +1,34 @@
+//
+//  MADViewController.m
+//  beatles
+//
+//  Created by Elise J Beall on 9/13/12.
+//  Copyright (c) 2012 Elise J Beall. All rights reserved.
+//
+
+#import "MADViewController.h"
+
+@interface MADViewController ()
+
+@end
+
+@implementation MADViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
