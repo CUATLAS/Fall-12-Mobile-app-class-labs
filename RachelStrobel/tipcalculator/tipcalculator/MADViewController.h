dc85d74062dd4083ca65bd4548efdb9792f84dab
Rachel Strobel <rachel.k.strobel@colorado.edu>
2012-09-18 12:11:12 -0600

    tipcalculator_final

---
+++ RachelStrobel/tipcalculator/tipcalculator/MADViewController.h
@@ -0,0 +1,13 @@
+//
+//  MADViewController.h
+//  tipcalculator
+//
+//  Created by Rachel Strobel on 9/18/12.
+//  Copyright (c) 2012 Rachel Strobel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MADViewController : UIViewController
+
+@end
