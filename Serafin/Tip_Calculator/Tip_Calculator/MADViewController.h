98d986654fc5b84daf41282440aa72ca4cc7b4c4
Scott <scottserafin@me.com>
2012-09-18 09:27:46 -0600

    Tip Calculator (Beginning)
    The initial save of the Tip Calculator In Class Lab

---
+++ Serafin/Tip_Calculator/Tip_Calculator/MADViewController.h
@@ -0,0 +1,13 @@
+//
+//  MADViewController.h
+//  Tip_Calculator
+//
+//  Created by Scott Serafin on 9/18/12.
+//  Copyright (c) 2012 Scott Serafin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MADViewController : UIViewController
+
+@end
