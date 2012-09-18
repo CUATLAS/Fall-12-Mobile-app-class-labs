16640132d9b5877e13648f6d235717274005cb7a
Rachel Strobel <rachel.k.strobel@colorado.edu>
2012-09-18 12:52:25 -0600

    hi

--- RachelStrobel/tipcalculator/tipcalculator/MADViewController.h
+++ RachelStrobel/tipcalculator/tipcalculator/MADViewController.h
@@ -8,6 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface MADViewController : UIViewController
+@interface MADViewController : UIViewController <UITextFieldDelegate, UIAlertViewDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *checkAmount;
+@property (weak, nonatomic) IBOutlet UITextField *tipPercent;
+@property (weak, nonatomic) IBOutlet UITextField *people;
+@property (weak, nonatomic) IBOutlet UILabel *tipDue;
+@property (weak, nonatomic) IBOutlet UILabel *totalDue;
+@property (weak, nonatomic) IBOutlet UILabel *totalDuePerPerson;
 
 @end
