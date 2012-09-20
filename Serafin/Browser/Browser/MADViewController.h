611d41c98fa9455c9194fd94fb7d9285800f0f6f
Scott <scottserafin@me.com>
2012-09-20 09:24:32 -0600

    New Class Project - Browser

---
+++ Serafin/Browser/Browser/MADViewController.h
@@ -0,0 +1,13 @@
+//
+//  MADViewController.h
+//  Browser
+//
+//  Created by Scott Serafin on 9/20/12.
+//  Copyright (c) 2012 Scott Serafin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MADViewController : UIViewController
+
+@end
---
+++ Serafin/Browser/Browser/MADViewController.m
@@ -0,0 +1,29 @@
+//
+//  MADViewController.m
+//  Browser
+//
+//  Created by Scott Serafin on 9/20/12.
+//  Copyright (c) 2012 Scott Serafin. All rights reserved.
+//
+
+#import "MADViewController.h"
+
+@interface MADViewController ()
+
+@end
+
+@implementation MADViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
