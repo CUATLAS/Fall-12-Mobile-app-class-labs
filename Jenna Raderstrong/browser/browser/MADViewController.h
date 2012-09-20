60f04402759299b57c31238f21666b9900d16b94
Jenna Raderstrong <jenraders@gmail.com>
2012-09-20 10:34:10 -0600

    browser

---
+++ Jenna Raderstrong/browser/browser/MADViewController.h
@@ -0,0 +1,16 @@
+//
+//  MADViewController.h
+//  browser
+//
+//  Created by Jenna Raderstrong on 9/20/12.
+//  Copyright (c) 2012 Jenna Raderstrong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MADViewController : UIViewController <UIWebViewDelegate, UIActionSheetDelegate>
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (weak, nonatomic) IBOutlet UIToolbar *toolBar;
+- (IBAction)bookmarkItemTapped:(UIBarButtonItem *)sender;
+
+@end
