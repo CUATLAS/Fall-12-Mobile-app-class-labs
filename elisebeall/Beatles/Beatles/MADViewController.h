2f1d2d2d60279d7d28eb112936fb69ed44a1e819
Elise Beall <elisebeall@gmail.com>
2012-09-26 18:41:35 -0600

    2012.09.26

---
+++ elisebeall/Beatles/Beatles/MADViewController.h
@@ -0,0 +1,13 @@
+//
+//  MADViewController.h
+//  Beatles
+//
+//  Created by Elise J Beall on 9/25/12.
+//  Copyright (c) 2012 Elise J Beall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MADViewController : UIViewController
+
+@end
