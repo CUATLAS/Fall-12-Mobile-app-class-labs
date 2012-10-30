364c7b716a4d3f4b443b6546a6a9caf12ed0d137
mustafa naseem <mustafanaseem@gmail.com>
2012-10-30 09:10:25 -0600

    countries2 - nonfinal commit (buggy)

---
+++ mustafa naseem/Countries2/Countries2/HWLAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  HWLAppDelegate.h
+//  Countries2
+//
+//  Created by  on 10/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HWLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UINavigationController *navController;
+
+@end
