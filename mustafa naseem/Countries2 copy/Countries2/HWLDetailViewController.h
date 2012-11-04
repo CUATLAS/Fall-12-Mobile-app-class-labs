e5999a53469c303a48e633e0a43ddc43eab8575a
mustafanaseem <mustafanaseem@gmail.com>
2012-11-03 20:07:49 -0600

    countries 2

---
+++ mustafa naseem/Countries2 copy/Countries2/HWLDetailViewController.h
@@ -0,0 +1,15 @@
+//
+//  HWLDetailViewController.h
+//  Countries2
+//
+//  Created by  on 10/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HWLDetailViewController : UITableViewController
+
+@property (strong, nonatomic) NSMutableArray *countryList;
+
+@end
---
+++ mustafa naseem/Countries2 copy/HWLMasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  HWLMasterViewController.h
+//  Countries2
+//
+//  Created by  on 10/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HWLMasterViewController : UITableViewController
+
+@end
