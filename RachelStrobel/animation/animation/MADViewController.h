5f1bb8f81fbabd7cdd12f1d93f3af3acd1329aea
Rachel Strobel <rachel.k.strobel@colorado.edu>
2012-11-27 22:16:51 -0700

    animation_rachels_final

---
+++ RachelStrobel/animation/animation/MADViewController.h
@@ -0,0 +1,17 @@
+//
+//  MADViewController.h
+//  animation
+//
+//  Created by Rachel Strobel on 11/15/12.
+//  Copyright (c) 2012 Rachel Strobel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MADViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UISlider *slider;
+@property (weak, nonatomic) IBOutlet UILabel *sliderLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+- (IBAction)sliderMoved:(UISlider *)sender;
+
+@end
