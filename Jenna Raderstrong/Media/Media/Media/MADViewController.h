c492b42408d71460c6cfbf64dc71bc80cab745b1
Aileen J Pierce <aileen.pierce@colorado.edu>
2012-12-17 16:13:30 -0700

    grading

--- Jenna Raderstrong/Media/Media/Media/MADViewController.h
+++ Jenna Raderstrong/Media/Media/Media/MADViewController.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <MobileCoreServices/MobileCoreServices.h>
+#import "MobileCoreServices/MobileCoreServices.h"
 #import "MediaPlayer/MediaPlayer.h"
 #import "AVFoundation/AVFoundation.h"
 
